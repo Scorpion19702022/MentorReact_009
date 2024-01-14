e853808fab7b2c81ddeabfb9e828509b631be588
skorpion <jmosinski@o2.pl>
2024-01-14 18:16:37 +0100

    order in components and added name styles css

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
 	return (
 		<div className={styles.App}>
 			<QueryClientProvider client={queryClient}>
-				<RecipesList />
 				<NavBar />
+				<RecipesList />
 			</QueryClientProvider>
 		</div>
 	)
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,16 +4,21 @@ import DarkModeIcon from '@mui/icons-material/DarkMode'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import Logo from './assets/przepisy.png'
+import styles from './NavBar.module.css'
 
 const NavBar = () => {
 	return (
-		<div>
-			<img src={Logo} alt='logo' />
-			<div>
+		<div className={styles.navBar}>
+			<div className={styles.boxNavLogo}>
+				<img className={styles.logo} src={Logo} alt='logo' />
+			</div>
+			<div className={styles.boxNavIcon}>
 				<DarkModeIcon />
 				<LightModeIcon />
 			</div>
-			<FavoriteBorderIcon />
+			<div className={styles.boxNavFavorite}>
+				<FavoriteBorderIcon />
+			</div>
 		</div>
 	)
 }
