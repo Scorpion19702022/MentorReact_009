6de43405d520a30c24bb91000876cccd006898c5
skorpion <jmosinski@o2.pl>
2024-01-14 19:15:37 +0100

    change logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
-import Logo from './assets/foodLogo.png'
+import Logo from './assets/logoFood.png'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
