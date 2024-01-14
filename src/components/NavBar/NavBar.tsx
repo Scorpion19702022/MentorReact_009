ac26d08d6266d87471018309e63d9e21a38cbcc1
skorpion <jmosinski@o2.pl>
2024-01-14 18:07:12 +0100

    added picture

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import Logo from './assets/przepisy.png'
 
 const NavBar = () => {
 	return (
 		<div>
-			<span>LOGO</span>
+			<img src={Logo} alt='logo' />
 			<div>
 				<DarkModeIcon />
 				<LightModeIcon />
