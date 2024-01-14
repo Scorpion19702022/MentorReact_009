8e0a9d6d576eeb7e49222e8af2872b7623d76e90
skorpion <jmosinski@o2.pl>
2024-01-14 19:38:01 +0100

    added styles

--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios'
 import { useQuery } from 'react-query'
 
+import styles from './RecipesList.module.css'
+
 const RecipesList = () => {
 	const fetchReiceps = async () => {
 		const res = await axios.get('https://recipeapp-4edaa-default-rtdb.europe-west1.firebasedatabase.app/Recipes.json')
@@ -13,7 +15,7 @@ const RecipesList = () => {
 	console.log(data)
 
 	return (
-		<div>
+		<div className={styles.boxRecipesList}>
 			<h2>Lista przepisów</h2>
 		</div>
 	)
