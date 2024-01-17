47d7c0636f0acae309d052c960574add868864d0
skorpion <jmosinski@o2.pl>
2024-01-17 20:50:45 +0100

    added content and styles

--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -19,14 +19,17 @@ const RecipesList = () => {
 			<img className={styles.img} src={recipe.img} alt='' />
 			<h3 className={styles.title}>{recipe.meal}</h3>
 			<h5 className={styles.recipeCategory}>{recipe.category}</h5>
-			<ul className={styles.recipeIngredients}>
-				<li className={styles.ingredient}>{recipe.ingrendients[1]}</li>
-				<li className={styles.ingredient}>{recipe.ingrendients[2]}</li>
-				<li className={styles.ingredient}>{recipe.ingrendients[3]}</li>
-			</ul>
+			<div className={styles.ingredients}>
+				<em className={styles.titleIngredients}>składniki:</em>
+				<ul className={styles.recipeIngredients}>
+					<li className={styles.ingredient}>{recipe.ingrendients[1]}</li>
+					<li className={styles.ingredient}>{recipe.ingrendients[2]}</li>
+					<li className={styles.ingredient}>{recipe.ingrendients[3]}</li>
+				</ul>
+			</div>
 			<p className={styles.method}>{recipe.methodOfPreparing}</p>
 			<p className={styles.rating}>
-				rating: <span className={styles.countRating}>{recipe.rating}</span>
+				rating: <span className={styles.countRating}> {recipe.rating}</span>
 			</p>
 			<em className={styles.author}>{recipe.author}</em>
 		</div>
