fa08fda9b38f067cdcc57c682a57fe66d7f032f1
skorpion <jmosinski@o2.pl>
2024-01-19 20:32:14 +0100

    change icons in navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,6 +6,10 @@ import React from 'react'
 import Logo from './assets/logo_ingredients.png'
 import styles from './NavBar.module.css'
 
+import DarkIcon from './assets/dark_icon.png'
+import SunIcon from './assets/sun_icon2.png'
+import HeartIcon from './assets/heart_icon.png'
+
 const NavBar = () => {
 	return (
 		<div className={styles.navBar}>
@@ -15,8 +19,13 @@ const NavBar = () => {
 			<div className={styles.boxNavIcon}>
 				{/* <DarkModeIcon className={styles.iconDark} style={{ width: '50px', height: '50px' }} />
 				<LightModeIcon className={styles.iconLight} /> */}
+				<img className={styles.iconDark} src={DarkIcon} alt='dark icon' />
+				<img className={styles.iconLight} src={SunIcon} alt='sun icon' />
 			</div>
-			<div className={styles.boxNavFavorite}>{/* <FavoriteBorderIcon className={styles.iconFavorite} /> */}</div>
+			<div className={styles.boxNavFavorite}>
+				{/* <FavoriteBorderIcon className={styles.iconFavorite} /> */}
+				<img className={styles.iconFavourite} src={HeartIcon} alt='favourite icon' />
+			</div>
 		</div>
 	)
 }
