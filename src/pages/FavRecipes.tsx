a85b5a3628e39f416c6b8971135a22645931587c
skorpion <jmosinski@o2.pl>
2024-02-02 20:04:23 +0100

    test

--- src/pages/FavRecipes.tsx
+++ src/pages/FavRecipes.tsx
@@ -3,14 +3,16 @@ import WishListContext from '../Context/WishListContext'
 import FavRecipesItem from '../components/FavRecipesItem'
 import { RecipeType } from '../types/MyTypes'
 
+import styles from '../components/FavRecipesItem.module.css'
+
 const FavRecipes = () => {
 	const { favRecipes } = useContext(WishListContext)
 
 	console.log(favRecipes)
 
 	return (
-		<div>
-			<h2>Favourite Lists</h2>
+		<div className={styles.wrapperFavRec}>
+			<h2 className={styles.favHeding}>Favourite Lists</h2>
 			{favRecipes.map((item: RecipeType) => {
 				return <FavRecipesItem favRecipeInfo={item} />
 			})}
