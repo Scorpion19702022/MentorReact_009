00684fed7283a148ddb0e21b4f5a15858978a975
skorpion <jmosinski@o2.pl>
2024-02-02 19:12:40 +0100

    added css for button in card recipe

--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -55,9 +55,9 @@ const RecipesList = () => {
 				rating: <span className={styles.countRating}> {recipe.rating}</span>
 			</p>
 			<em className={styles.author}>{recipe.author}</em>
-			<div>
-				<button onClick={() => addToWishList(recipe)}>Dodaj do ulubioych</button>
-			</div>
+			<button className={styles.btnAddWish} onClick={() => addToWishList(recipe)}>
+				Dodaj do ulubioych
+			</button>
 		</div>
 	))
 
