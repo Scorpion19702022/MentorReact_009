b81329e0384fa27a4632824b72e8be605bb115b2
skorpion <jmosinski@o2.pl>
2024-02-02 19:52:57 +0100

    added structure in component FavRecipesItem

--- src/components/FavRecipesItem.tsx
+++ src/components/FavRecipesItem.tsx
@@ -13,6 +13,22 @@ const FavRecipesItem: React.FC<favRecipeItemProps> = ({ favRecipeInfo }) => {
 		<div className={styles.favRecipesItemBox}>
 			<div className={styles.favRecipeCard}>
 				<h2 className={styles.favRecipeTitle}>{favRecipeInfo.meal}</h2>
+				<h4 className={styles.favRecipeCategory}>{favRecipeInfo.category}</h4>
+				<div className={styles.favRecipeIngrendientsBox}>
+					{favRecipeInfo.ingrendients.map((i: any) => {
+						if (i === null) {
+							return null
+						}
+						return (
+							<li key={i}>
+								<ul>{i}</ul>
+							</li>
+						)
+					})}
+				</div>
+				<p>{favRecipeInfo.author}</p>
+				<em>{favRecipeInfo.rating}</em>
+				<button>delete</button>
 			</div>
 		</div>
 	)
