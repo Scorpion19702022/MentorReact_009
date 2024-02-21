4f12b2f8de8d49cf1fe8742037943c98428dd704
skorpion <jmosinski@o2.pl>
2024-02-21 20:40:24 +0100

    test

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -7,6 +7,8 @@ import { RecipeType } from '../types/MyTypes'
 import RecipeItem from '../components/RecipeItem'
 import { useEffect, useState } from 'react'
 
+import styles from '../components/Category.module.css'
+
 const Category = () => {
 	const location = useLocation()
 
@@ -18,7 +20,7 @@ const Category = () => {
 
 	const { recipes } = useFetch()
 
-	console.log(recipes)
+	// console.log(recipes)
 
 	// console.log(recipes)
 
@@ -31,17 +33,19 @@ const Category = () => {
 	}, [recipes, urlCategoryRecipe])
 
 	return (
-		<div>
+		<div className={styles.wrapper_category}>
 			<NavBar />
-			<CategoryLink />
-			<SearchForm />
-			<h1>
-				Category: {urlCategoryRecipe} ({result?.length})
-			</h1>
-			<div>
-				{result?.map((recipe: RecipeType) => {
-					return <RecipeItem recipeInfo={recipe} />
-				})}
+			<div className={styles.content_category}>
+				<CategoryLink />
+				<SearchForm />
+				<h1>
+					Category: {urlCategoryRecipe} ({result?.length})
+				</h1>
+				<div>
+					{result?.map((recipe: RecipeType) => {
+						return <RecipeItem recipeInfo={recipe} />
+					})}
+				</div>
 			</div>
 		</div>
 	)
